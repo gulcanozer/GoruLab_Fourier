83b5de3eb1bfb123c9eadbbf02f988ccb40a82b7
Gulcan Ozer <gulcan.ozer@bil.omu.edu.tr>
2013-04-27 18:37:48 +0300

    gerceklendi

---
+++ 2)proje/extract_features.m
@@ -0,0 +1,27 @@
+%% documentation 
+%
+%
+function data = extract_features(bw,dbg)
+    bw = bw(1:40,:);
+    
+    dis_kontur = edge(bw,'sobel');
+    
+    f = fft2(bw);
+    F = fftshift(f); 
+    
+    df = F.*bw; 
+    
+    data = imagesc(20*log10(abs(fftshift(f))));
+    
+    %if dbg
+        %figure(1);
+            subplot(221), imshow(bw)
+            subplot(222), imshow(dis_kontur)
+            subplot(223), imshow(log(abs(df)),[])
+            subplot(224), imagesc(log(abs(F)))
+   % end
+end
+
+%% helpers funcs
+%
+%
