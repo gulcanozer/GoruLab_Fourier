d372de4306aa695d3ae258ab2e11c914cb7d421d
Özlem Okumus <ozlem.okumus@bil.omu.edu.tr>
2013-04-28 21:19:15 +0300

    Update extract_features.m

--- Fourier/2)proje/extract_features.m
+++ Fourier/2)proje/extract_features.m
@@ -7,14 +7,15 @@ function data = extract_features(bw,dbg)
     dis_kontur = edge(bw,'sobel'); %% Görüntünün kenarlarını bulur
     
     f = fft2(bw); %% Görüntüye fourier uygular
-    F = fftshift(f); %% 
+    F = fftshift(f); %% Görüntüyü netleştirmek için sıfır olan frekans bileşenini yeniden düzenler
     
-    df = F.*bw; 
+    df = F.*bw; %% Maske ile görüntüyü çarpar
     
-    data = imagesc(20*log10(abs(fftshift(f))));
+    data = imagesc(20*log10(abs(fftshift(f)))); %% Fourieri alınmış görüntüyü düzenler
     
     %if dbg
         %figure(1);
+%% Aynı pencerede çoklu çizim yapar
             subplot(221), imshow(bw)
             subplot(222), imshow(dis_kontur)
             subplot(223), imshow(log(abs(df)),[])
